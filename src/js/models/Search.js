d6c3f0369929d03191ccd82ef6fee0c8822ea01e
leonyangela <leonyangela@DESKTOP-1KNN1EJ>
2020-03-30 20:18:25 +0700

    Build the Search MVC, Loading Spinner, & Search Pagination.

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = res.data.recipes;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+// similiar to food2fork api
+// https://api.spoonacular.com/recipes/search
