6fafb4fcede44a2bc7255e2754c77df57688af5b
leonyangela <leonyangela@DESKTOP-1KNN1EJ>
2020-04-04 18:34:45 +0700

    Save data using localStorage

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -8,12 +8,18 @@ export default class Likes {
         const like = { id, title, author, img };
         this.likes.push(like);
 
+        // Persist data in local storage
+        this.persistData();
+
         return like;
     }
 
     deleteLike(id) {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
+
+        // Persist data in local storage
+        this.persistData();
     }
 
     isLiked(id) {
@@ -22,5 +28,16 @@ export default class Likes {
 
     getNumLikes() {
         return this.likes.length;
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes'));
+
+        // Restoring likes from the localStorage
+        if (storage) this.likes = storage;
     }
 }
